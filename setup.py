3e092ebd113da250e3b7196205f512c284fbee4a
JeanMichel FRANCOIS <toutpt@gmail.com>
2011-12-02 12:07:52 +0100

    rename README.txt -> REAMDE.rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0a3dev'
 setup(name='collective.jqueryuithememanager',
       version=version,
       description="JQueryUI theme manager for Plone",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
